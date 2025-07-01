f2424c95d13e2d696210230f5782ba774f717924
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-07-01 22:45:53 +0100

    Add Prettier to pre-commit linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,12 @@
 export default {
-  '*.js': ['npm run lint:js:cli -- --fix'],
-  '*.scss': ['npm run lint:scss -- --fix']
+  '*.js': [
+    'npm run lint:js:cli -- --fix',
+    'npm run lint:prettier:cli -- --write'
+  ],
+  '*.scss': [
+    'npm run lint:scss -- --fix',
+    'npm run lint:prettier:cli -- --write'
+  ],
+  '*.md': ['npm run lint:prettier:cli -- --write'],
+  '*.{yaml,yml}': ['npm run lint:prettier:cli -- --write']
 }
