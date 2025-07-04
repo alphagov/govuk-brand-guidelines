f0c93044339201f58ee442a2fadd0b8821784266
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-07-04 10:49:39 +0100

    Set up ESLint Promise plugin
    This will help us use good practices around Promises

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import neostandard from 'neostandard'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import pluginPromise from 'eslint-plugin-promise'
 
 export default [
   ...neostandard({
     noStyle: true
   }),
+  pluginPromise.configs['flat/recommended'],
   eslintConfigPrettier
 ]
