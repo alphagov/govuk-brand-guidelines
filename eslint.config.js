2f8a6be3d505795b2f4d9e6892defff13423181e
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-07-04 10:51:03 +0100

    Ignore files not commited to the repository when linting JS
    Following ESLint's documentation: https://eslint.org/docs/latest/use/configure/ignore#including-gitignore-files

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
+import { join } from 'node:path'
 import neostandard from 'neostandard'
+import { includeIgnoreFile } from '@eslint/compat'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import pluginPromise from 'eslint-plugin-promise'
 
+const gitignorePath = join(import.meta.dirname, '.gitignore')
+
 export default [
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   ...neostandard({
     noStyle: true
   }),
