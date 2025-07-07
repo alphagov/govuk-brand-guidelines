6a881c1de186ae1065d914972dc9e415722292e9
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2025-07-07 17:15:45 +0100

    Fix configuration of @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -5,22 +5,30 @@
  */
 
 export default function (api) {
+  // If build gets too slow we can revisit, but rather
+  // than handling the complexity of managing the caching
+  // of Babel configuration and risking issues that are hard
+  // to debug, let's have Babel re-run the config for now
+  api.cache(false)
+
   return {
     presets: [
-      '@babel/preset-env',
-      {
-        // Use our JS support query specifically
-        browserslistEnv: 'javascript',
+      [
+        '@babel/preset-env',
+        {
+          // Use our JS support query specifically
+          browserslistEnv: 'javascript',
 
-        // Apply bug fixes to avoid transforms
-        bugfixes: true,
+          // Apply bug fixes to avoid transforms
+          bugfixes: true,
 
-        // Apply smaller "loose" transforms for browsers
-        loose: true,
+          // Apply smaller "loose" transforms for browsers
+          loose: true,
 
-        // Skip ES module transforms for browsers
-        modules: false
-      }
+          // Skip ES module transforms for browsers
+          modules: false
+        }
+      ]
     ]
   }
 }
