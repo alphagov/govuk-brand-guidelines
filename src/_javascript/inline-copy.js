e0585a907d850686a4cc4aaab40997f373aebf58
beeps <hi+github@berly.kim>
2025-09-02 14:57:35 +0100

    Update copy button design
    - Expand the button to include and replace the value to be copied.
    - Redesign button appearance
    - Update iconography
    - Refactor button JS to split out states
    
    Not sure how accessible this is

--- src/_javascript/inline-copy.js
+++ src/_javascript/inline-copy.js
@@ -1,22 +1,24 @@
 import { ConfigurableComponent } from 'govuk-frontend'
 
-// Contents of button prior to being clicked
-const copyButtonHtml = `
-<svg focusable="false" aria-hidden="true" viewBox="0 0 28 28" width="28" height="28">
-  <path d="M12 26h14v-10h-6.5c-0.828 0-1.5-0.672-1.5-1.5v-6.5h-6v18zM16 3.5v-1c0-0.266-0.234-0.5-0.5-0.5h-11c-0.266 0-0.5 0.234-0.5 0.5v1c0 0.266 0.234 0.5 0.5 0.5h11c0.266 0 0.5-0.234 0.5-0.5zM20 14h4.672l-4.672-4.672v4.672zM28 16v10.5c0 0.828-0.672 1.5-1.5 1.5h-15c-0.828 0-1.5-0.672-1.5-1.5v-2.5h-8.5c-0.828 0-1.5-0.672-1.5-1.5v-21c0-0.828 0.672-1.5 1.5-1.5h17c0.828 0 1.5 0.672 1.5 1.5v5.125c0.203 0.125 0.391 0.266 0.562 0.437l6.375 6.375c0.594 0.594 1.062 1.734 1.062 2.562z"></path>
-</svg>
-<span class="govuk-visually-hidden">Copy</span>`
+const copyIcon = `
+<svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
+  <polygon points="12.58 16.51 2.27 16.51 2.27 3.35 3.27 3.35 3.27 15.51 12.58 15.51 12.58 16.51"/>
+  <path d="M15.73,13.91H4.88V1.49h10.86v12.42ZM5.88,12.91h8.86V2.49H5.88v10.42Z"/>
+</svg>`
 
-// Contents of button when copy has taken place
-const copyButtonCopiedHtml = `
-<svg focusable="false" aria-hidden="true" viewBox="0 0 28 28" width="28" height="28">
-  <path d="M26.109 8.844c0 0.391-0.156 0.781-0.438 1.062l-13.438 13.438c-0.281 0.281-0.672 0.438-1.062 0.438s-0.781-0.156-1.062-0.438l-7.781-7.781c-0.281-0.281-0.438-0.672-0.438-1.062s0.156-0.781 0.438-1.062l2.125-2.125c0.281-0.281 0.672-0.438 1.062-0.438s0.781 0.156 1.062 0.438l4.594 4.609 10.25-10.266c0.281-0.281 0.672-0.438 1.062-0.438s0.781 0.156 1.062 0.438l2.125 2.125c0.281 0.281 0.438 0.672 0.438 1.062z"></path>
-</svg>
-<span class="govuk-visually-hidden">Copied!</span>`
+const checkmarkIcon = `
+<svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
+  <path d="M9,1C4.58,1,1,4.58,1,9s3.58,8,8,8,8-3.58,8-8S13.42,1,9,1ZM14.33,6.84l-6.66,6.66c-.06.06-.15.06-.21,0l-3.78-3.78c-.06-.06-.06-.15,0-.21l.92-.92c.06-.06.15-.06.21,0l2.76,2.76,5.64-5.64c.06-.06.15-.06.21,0l.92.92c.06.06.06.15,0,.21Z"/>
+</svg>`
 
 export class InlineCopy extends ConfigurableComponent {
+  $button
+  initialText
+
   constructor($root, config) {
     super($root, config)
+
+    this.initialText = $root.innerHTML
 
     // Create copy button
     const $button = document.createElement('button')
@@ -24,37 +26,41 @@ export class InlineCopy extends ConfigurableComponent {
     if (this.config.buttonClass) {
       $button.className = `${this.config.buttonClass}`
     }
-    $button.innerHTML = copyButtonHtml
-    $button.setAttribute('aria-label', `Copy ${this.config.copyValue}`)
-    $button.setAttribute('aria-live', 'assertive')
     $button.addEventListener(
       'click',
       async () => await this.copy($button, this.config.copyValue)
     )
+    this.$button = $button
+    this.resetButton()
 
-    $root.insertAdjacentElement('beforeend', $button)
+    $root.removeChild($root.firstChild)
+    $root.appendChild($button)
+  }
+
+  resetButton() {
+    this.$button.setAttribute('aria-label', `Copy ${this.config.copyValue}`)
+    this.$button.innerHTML = `${this.initialText} ${copyIcon}`
+    this.$button.classList.remove(
+      this.config.buttonClass ? `${this.config.buttonClass}--copied` : 'copied'
+    )
+  }
+
+  activeButton() {
+    this.$button.setAttribute('aria-label', `Copied!`)
+    this.$button.innerHTML = `${this.initialText} ${checkmarkIcon}`
+    this.$button.classList.add(
+      this.config.buttonClass ? `${this.config.buttonClass}--copied` : 'copied'
+    )
   }
 
   async copy($button, text) {
     try {
       await navigator.clipboard.writeText(text)
-
-      // Display confirmation in the button
-      $button.innerHTML = copyButtonCopiedHtml
-      $button.classList.add(
-        this.config.buttonClass
-          ? `${this.config.buttonClass}--copied`
-          : 'copied'
-      )
+      this.activeButton()
 
       // Begin a timer to reset the button contents
       setTimeout(() => {
-        $button.innerHTML = copyButtonHtml
-        $button.classList.remove(
-          this.config.buttonClass
-            ? `${this.config.buttonClass}--copied`
-            : 'copied'
-        )
+        this.resetButton()
       }, this.config.delay)
     } catch (error) {
       console.error(error.message)
