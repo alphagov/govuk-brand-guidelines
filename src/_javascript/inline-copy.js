82b92b13e013180825c12e826dee65f3bb2f2251
beeps <hi+github@berly.kim>
2025-09-04 15:35:03 +0100

    Improve screen reader announcements
    - Use a separate status element for 'copied' announcement
    - Reduce reset time between button activations

--- src/_javascript/inline-copy.js
+++ src/_javascript/inline-copy.js
@@ -1,24 +1,46 @@
 import { ConfigurableComponent } from 'govuk-frontend'
 
-const copyIcon = `
-<svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
-  <polygon points="12.58 16.51 2.27 16.51 2.27 3.35 3.27 3.35 3.27 15.51 12.58 15.51 12.58 16.51"/>
-  <path d="M15.73,13.91H4.88V1.49h10.86v12.42ZM5.88,12.91h8.86V2.49H5.88v10.42Z"/>
-</svg>`
+const copyButtonHtml = (
+  colourValue
+) => `<span class="govuk-visually-hidden">Copy</span>
+  ${colourValue}
+  <svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
+    <polygon points="12.58 16.51 2.27 16.51 2.27 3.35 3.27 3.35 3.27 15.51 12.58 15.51 12.58 16.51"/>
+    <path d="M15.73,13.91H4.88V1.49h10.86v12.42ZM5.88,12.91h8.86V2.49H5.88v10.42Z"/>
+  </svg>`
 
-const checkmarkIcon = `
-<svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
-  <path d="M9,1C4.58,1,1,4.58,1,9s3.58,8,8,8,8-3.58,8-8S13.42,1,9,1ZM14.33,6.84l-6.66,6.66c-.06.06-.15.06-.21,0l-3.78-3.78c-.06-.06-.06-.15,0-.21l.92-.92c.06-.06.15-.06.21,0l2.76,2.76,5.64-5.64c.06-.06.15-.06.21,0l.92.92c.06.06.06.15,0,.21Z"/>
-</svg>`
+const copiedButtonHtml = (
+  colourValue
+) => `<span class="govuk-visually-hidden">Copy</span>
+  ${colourValue}
+  <svg focusable="false" aria-hidden="true" viewBox="0 0 18 18" width="18" height="18">
+    <path d="M9,1C4.58,1,1,4.58,1,9s3.58,8,8,8,8-3.58,8-8S13.42,1,9,1ZM14.33,6.84l-6.66,6.66c-.06.06-.15.06-.21,0l-3.78-3.78c-.06-.06-.06-.15,0-.21l.92-.92c.06-.06.15-.06.21,0l2.76,2.76,5.64-5.64c.06-.06.15-.06.21,0l.92.92c.06.06.06.15,0,.21Z"/>
+  </svg>`
 
 export class InlineCopy extends ConfigurableComponent {
   $button
+  $status
   initialText
 
   constructor($root, config) {
     super($root, config)
-
     this.initialText = $root.innerHTML
+
+    // Pages can have a lot (hundreds!) of copy buttons on them. Instead of
+    // having a screen reader announcement element for each one like on the
+    // Design System website, they all share one status element.
+    this.$status = document.querySelector('[data-inline-copy-status]')
+
+    // If there is no status element, create one
+    if (!this.$status) {
+      const $status = document.createElement('div')
+      $status.className = 'govuk-visually-hidden'
+      $status.setAttribute('aria-live', 'assertive')
+      $status.setAttribute('data-inline-copy-status', '')
+      document.body.insertAdjacentElement('beforeend', $status)
+
+      this.$status = $status
+    }
 
     // Create copy button
     const $button = document.createElement('button')
@@ -33,21 +55,26 @@ export class InlineCopy extends ConfigurableComponent {
     this.$button = $button
     this.resetButton()
 
-    $root.removeChild($root.firstChild)
+    // Remove all child elements
+    while ($root.firstChild) {
+      $root.removeChild($root.firstChild)
+    }
+
+    // Insert button
     $root.appendChild($button)
   }
 
   resetButton() {
-    this.$button.setAttribute('aria-label', `Copy ${this.config.copyValue}`)
-    this.$button.innerHTML = `${this.initialText} ${copyIcon}`
+    this.$button.innerHTML = copyButtonHtml(this.initialText)
+    this.$status.innerText = ''
     this.$button.classList.remove(
       this.config.buttonClass ? `${this.config.buttonClass}--copied` : 'copied'
     )
   }
 
   activeButton() {
-    this.$button.setAttribute('aria-label', `Copied!`)
-    this.$button.innerHTML = `${this.initialText} ${checkmarkIcon}`
+    this.$button.innerHTML = copiedButtonHtml(this.initialText)
+    this.$status.innerText = 'Copied!'
     this.$button.classList.add(
       this.config.buttonClass ? `${this.config.buttonClass}--copied` : 'copied'
     )
@@ -79,7 +106,7 @@ export class InlineCopy extends ConfigurableComponent {
 
   static defaults = {
     copyValue: '',
-    delay: 3000,
+    delay: 1500,
     buttonClass: 'app-inline-copy'
   }
 
