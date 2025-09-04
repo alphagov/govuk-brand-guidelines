779e832eb42939a104ae21a57452a8cc16857e21
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2025-09-04 17:30:18 +0100

    Add inform inspire component to replace homepage graphic

---
+++ eleventy/shortcodes/inform-inspire.js
@@ -0,0 +1,23 @@
+import { blockPairedShortcode } from './utils.js'
+
+// Shortcode for the common visual styling around the numerous Inform Inspire
+// graphics.
+export const informInspire = blockPairedShortcode((content, options = {}) => {
+  const defaultOptions = {
+    classes: undefined,
+    headingLevel: 2
+  }
+  options = { ...defaultOptions, ...options }
+
+  return `<div class="app-inform-inspire${options.classes ? ` ${options.classes}` : ''}">
+    <h${options.headingLevel} class="govuk-visually-hidden">From most informative to most inspiring</h${options.headingLevel}>
+    <div class="app-inform-inspire__axis" aria-hidden="true">
+      <div class="app-inform-inspire__axis-label">Inform</div>
+      <div class="app-inform-inspire__axis-dot"></div>
+      <div class="app-inform-inspire__axis-label">Inspire</div>
+    </div>
+    <div class="app-inform-inspire__content">${
+      content
+    }</div>
+  </div>`
+})
