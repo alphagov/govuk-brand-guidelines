d8407656667d10458f3b5b48e9fbf844f9c77b02
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2025-09-04 17:33:28 +0100

    Add list param to inform inspire shortcode for social colours graphic

--- eleventy/shortcodes/inform-inspire.js
+++ eleventy/shortcodes/inform-inspire.js
@@ -2,11 +2,22 @@ import { blockPairedShortcode } from './utils.js'
 
 // Shortcode for the common visual styling around the numerous Inform Inspire
 // graphics.
+//
+// This component has 3 variants that we take into account with this shortcode:
+//
+// - No need for extra styling besides the standard container
+// - Modifiers on the content element eg: adding flex styling which we do via
+// `contentClasses`
+// - A list under the content element which we do via `list`. We handle the
+// rendering of the list here rather than in the markdown to avoid markdown
+// processing nonsense
 export const informInspire = blockPairedShortcode((content, options = {}) => {
   const defaultOptions = {
     classes: undefined,
     headingLevel: 3,
-    contentClasses: undefined
+    contentClasses: undefined,
+    list: [],
+    listLabel: undefined
   }
   options = { ...defaultOptions, ...options }
 
@@ -19,6 +30,14 @@ export const informInspire = blockPairedShortcode((content, options = {}) => {
     </div>
     <div class="app-inform-inspire__content${options.contentClasses ? ` ${options.contentClasses}` : ''}">${
       content
-    }</div>
-  </div>`
+    }</div>${
+      options.list.length > 0
+        ? `${options.listLabel
+          ? `<p class="govuk-body govuk-!-margin-top-4">${options.listLabel}</p>`
+          : ''}
+        <ul class="govuk-list govuk-list--bullet govuk-!-margin-bottom-0">
+        ${options.list.map((item) => `<li>${item}</li>`).join('\n')}
+      </ul>`
+        : ''
+    }</div>`
 })
