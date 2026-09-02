445ff6d04509e046c840973969a64bb910773203
BarneyL <Barney.Loosemore@dsit.gov.uk>
2026-09-02 12:16:57 +0100

    Add support for multiple images in figure shortcode & move to informinspire shortcode on illustration principles page

--- eleventy/shortcodes/figure.js
+++ eleventy/shortcodes/figure.js
@@ -8,17 +8,14 @@ export const figure = blockPairedShortcode((content, options = {}) => {
     // Image attributes
     // The resizing and conversion of images will be picked up by eleventy-img
     src: undefined,
-    alt: undefined
+    alt: undefined,
+
+    // For rendering multiple images described by a single figcaption
+    images: undefined
   }
   options = { ...defaultOptions, ...options }
 
-  if (typeof options.alt === 'undefined') {
-    console.error('Figure is missing a value for `src`.')
-  }
-
-  if (typeof options.alt === 'undefined') {
-    console.error('Figure is missing a value for `alt`.')
-  }
+  options.images ??= [{ src: options.src, alt: options.alt }]
 
   // Check caption position
   if (
@@ -31,14 +28,31 @@ export const figure = blockPairedShortcode((content, options = {}) => {
   // Assemble caption and image
   const captionHtml = `<figcaption class="app-figure__caption">${content}</figcaption>`
 
+  const imageHtml = options.images
+    .map(({ src, alt }) => {
+      checkImageAttributes(src, alt)
+      return `<img class="app-figure__image" alt="${alt}" src="${src}">`
+    })
+    .join('\n')
+
   // This is written like this because excess newlines tend to become jank
   // (i.e. unnecessary and/or empty paragraphs) in the Markdown processor.
   // It's easier to output this as one line of HTML.
   //
   // prettier-ignore
   return `<figure class="app-figure app-figure--caption-${options.captionPosition}${options.classes ? ` ${options.classes}` : ''}">${
-    options.captionPosition === "top" ? captionHtml : ''
-  }<img class="app-figure__image" alt="${options.alt}" src="${options.src}">${
-    options.captionPosition === "bottom" ? captionHtml : ''
+    options.captionPosition === 'top' ? captionHtml : ''
+  }<div class="app-figure__images-container">${imageHtml}</div>${
+    options.captionPosition === 'bottom' ? captionHtml : ''
   }</figure>`
 })
+
+function checkImageAttributes(src, alt) {
+  if (typeof src === 'undefined') {
+    console.error('Image in figure is missing a value for `src`.')
+  }
+
+  if (typeof alt === 'undefined') {
+    console.error('Image in figure is missing a value for `alt`.')
+  }
+}
